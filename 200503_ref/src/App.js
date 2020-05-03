7121ce0cea7da12951faf62bd4892ccafc6e07ff
Choi Kang Jae <48118981+ChoiKangJae@users.noreply.github.com>
2020-05-03 21:31:31 +0900

    200503_ref-1

---
+++ 200503_ref/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ValidationSample from './ValidationSample';
+
+function App() {
+  return (
+    <div className="App">
+      <ValidationSample></ValidationSample>
+    </div>
+  );
+}
+
+export default App;
