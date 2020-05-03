9f676450a9d443bbe47f8bfea5b3eb559d51ab35
Choi Kang Jae <48118981+ChoiKangJae@users.noreply.github.com>
2020-05-03 22:24:50 +0900

    200503_ref-2

--- 200503_ref/src/App.js
+++ 200503_ref/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
-function App() {
-  return (
-    <div className="App">
-      <ValidationSample></ValidationSample>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <ValidationSample></ValidationSample>
+        <div>
+          <ScrollBox
+            ref={ref => {
+              this.scrollBox = ref;
+            }}
+          ></ScrollBox>
+        </div>
+        <button
+          onClick={() => {
+            this.scrollBox.scrollToBottom();
+          }}
+        >
+          맨밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
