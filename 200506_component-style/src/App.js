95f97cee0ddfd4fbdcb86d7e979388f46e63f1cc
Choi Kang Jae <48118981+ChoiKangJae@users.noreply.github.com>
2020-05-06 18:01:19 +0900

    200506_component-style-3

--- 200506_component-style/src/App.js
+++ 200506_component-style/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponents';
 
 function App() {
   return (
     <div className="App">
       <SassComponent></SassComponent>
+      <CSSModule></CSSModule>
+      <StyledComponent></StyledComponent>
     </div>
   );
 }
