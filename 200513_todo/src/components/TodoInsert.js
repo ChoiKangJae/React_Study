c765c5c12d73bef777781efe4d2966b297ed6feb
Choi Kang Jae <48118981+ChoiKangJae@users.noreply.github.com>
2020-05-13 23:30:52 +0900

    200513_todo - 1

---
+++ 200513_todo/src/components/TodoInsert.js
@@ -0,0 +1,44 @@
+import React, { useState, useCallback } from 'react';
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(''); // value 값 초기화
+
+      // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다
+      // 이를 방지하기 위해 이 함수를 호출
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  const onClick = useCallback(() => {
+    onInsert(value);
+    setValue('');
+  }, [onInsert, value]);
+
+  return (
+    <form action="" className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit" onClick={onClick}>
+        <MdAdd></MdAdd>
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
