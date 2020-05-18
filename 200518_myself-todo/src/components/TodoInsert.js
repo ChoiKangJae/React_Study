74a4b8d1ba0165dafec6884b202a14d2d4d3a37a
Choi Kang Jae <48118981+ChoiKangJae@users.noreply.github.com>
2020-05-19 01:58:47 +0900

    200519_myself-todo - 2

---
+++ 200518_myself-todo/src/components/TodoInsert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoInsert.scss';
+
+function TodoInsert() {
+  return (
+    <div className="TodoInsert">
+      <input type="text" placeholder="What needs to be done?" />
+    </div>
+  );
+}
+
+export default TodoInsert;
