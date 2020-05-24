6cc1bb27222e4cd1d63eaf7a5d02161361fc11e8
Choi Kang Jae <48118981+ChoiKangJae@users.noreply.github.com>
2020-05-24 22:26:29 +0900

    200524_spa - 2

--- 200524_spa/src/App.js
+++ 200524_spa/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Profiles from './Profiles';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} exact={true}></Route>
+      <Route path={['/about', '/info']} component={About}></Route>
+      <Route path="/profiles" component={Profiles}></Route>
     </div>
   );
-}
+};
 
 export default App;
