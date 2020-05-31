8846de70519486b50ef9cc1d1bcd8639c01e7763
Choi Kang Jae <48118981+ChoiKangJae@users.noreply.github.com>
2020-05-31 19:19:57 +0900

    200531_spa - 3

--- 200524_spa/src/App.js
+++ 200524_spa/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -17,10 +18,26 @@ const App = () => {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">history</Link>
+        </li>
       </ul>
-      <Route path="/" component={Home} exact={true}></Route>
-      <Route path={['/about', '/info']} component={About}></Route>
-      <Route path="/profiles" component={Profiles}></Route>
+      <hr />
+      <Switch>
+        <Route path="/" component={Home} exact={true}></Route>
+        <Route path={['/about', '/info']} component={About}></Route>
+        <Route path="/profiles" component={Profiles}></Route>
+        <Route path="/history" component={HistorySample}></Route>
+        <Route
+          // path를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        ></Route>
+      </Switch>
     </div>
   );
 };
